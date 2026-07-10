8ae10dc5914e72a77bb578da8d972439d9b0874b
razenephyrz <reinzalkinas@gmail.com>
2026-07-10 19:34:32 +0700

    debugging domain/transaction.py

--- domain/transaction.py
+++ domain/transaction.py
@@ -16,17 +16,19 @@ class Transaction:
         date : datetime | None = None,
         desc : str | None = None
     ):
-        self._category_name = self.category_validator(category_name)
-        self._amount = self.amount_validator(amount)
-        self._type = self.type_validator(type)
-        self._date = self.date_validator(date)
-        self._desc = self.desc_validator(desc)
+        self._category_name = Transaction.category_validator(category_name)
+        self._amount = Transaction.amount_validator(amount)
+        self._type = Transaction.type_validator(type)
+        self._date = Transaction.date_validator(date)
+        self._desc = Transaction.desc_validator(desc)
     
+    @staticmethod
     def category_validator(value: str) -> str:
         if len(value) < 3:
             raise ValueError("Kategori terlalu pendek")
         return value.strip().lower()
     
+    @staticmethod
     def amount_validator(value: Decimal) -> Decimal:
         if not isinstance(value, Decimal):
             raise ValueError("Tidak boleh input selain angka")
@@ -34,19 +36,25 @@ class Transaction:
             raise ValueError("Angka tidak boleh 0 atau negatif")
         return value
     
+    @staticmethod
     def type_validator(value: str) -> TransactionType:
         tmp = value.strip().lower()
-        if tmp not in TransactionType:
-            raise ValueError("Tipe transaksi tidak valid")
-        return TransactionType(tmp)
+        try:
+            return TransactionType(tmp)
+        except ValueError:
+            print("Type tidak tersedia")
     
-    def date_validator(value: str) -> date | None:
+    @staticmethod
+    def date_validator(value: str | None) -> date | None:
+        if value is None:
+            return datetime.now()
         try:
             return datetime.fromisoformat(value)
         except ValueError:
-            print("Inputnya salah, kemungkinan di penulisan")
-            return None
+            print("Masukkan urutan waktu yang tepat")
+            return datetime.now()
         
+    @staticmethod 
     def desc_validator(value: str) -> str:
         if len(value) > 200:
             raise ValueError("Deskripsi tidak boleh lebih dari 200 huruf")
